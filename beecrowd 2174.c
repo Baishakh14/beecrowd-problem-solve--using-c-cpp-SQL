56fbead3973df4a70a4dfed7b1a6b70679471424
Baishakh14 <146338298+Baishakh14@users.noreply.github.com>
2023-12-20 04:49:58 +0600

    2174 - Pomekon Collection
    2174 - Pomekon Collection

---
+++ beecrowd 2174.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    unsigned n; ///only positive int.you can use unsigned int (also)
+    scanf("%u",&n);
+    char a[n][100];
+    int d = 0;
+    for(int i = 0;i<n;i++)
+    {
+        scanf("%s%*c",a[i]);
+    }
+    for(int i = 0;i<n;i++)
+    {
+    int c = 1;
+        for(int j = i+1;j<n;j++){
+                if(strcmp(a[i],a[j])==0){
+                c = 0;
+                break;
+                }
+        }
+        d+=c;
+    }
+    int p = 151-d;
+    printf("Falta(m) %d pomekon(s).\n",p);
+    return 0;
+}
+
