34f914bf46b431ecda493dd7528a6c9289fb0a36
Baishakh14 <146338298+Baishakh14@users.noreply.github.com>
2024-01-14 21:12:38 +0600

    2926 - So This is Christmas!
    2926 - So This is Christmas!

---
+++ beecrowd 2926.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  scanf("%d",&n);
+    char a[10000];
+    for(int i = 0;i<n;i++)
+    {
+        a[i] = 'a';
+    }
+    printf("Ent%so eh N%st%sl!\n",a,a,a);
+    return 0;
+}
+
