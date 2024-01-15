6e1367709d2b14fb5f5059c54b621d96668f32fd
Baishakh14 <146338298+Baishakh14@users.noreply.github.com>
2024-01-15 22:58:19 +0600

    2454 - Flíper
    2454 - Flíper

---
+++ beecrowd 2454.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int p,r;
+  scanf("%d %d",&p,&r);
+  if(!p) printf("C\n");
+  if(p==1)
+  {
+      if(r==0) printf("B\n");
+      if(r==1) printf("A\n");
+  }
+
+
+    return 0;
+}
+
