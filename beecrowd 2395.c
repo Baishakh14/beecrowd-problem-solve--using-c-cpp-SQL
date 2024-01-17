e56058648757b5a5522f0e5cb99ba0baa92b9faa
Baishakh14 <146338298+Baishakh14@users.noreply.github.com>
2024-01-17 21:07:11 +0600

    2395 - Transporte de Contêineres
    2395 - Transporte de Contêineres

---
+++ beecrowd 2395.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,c;
+   scanf("%d %d %d",&a,&b,&c);
+   int a1,b1,c1;
+   scanf("%d%d%d",&a1,&b1,&c1);
+   printf("%d\n",(a1/a)*(b1/b)*(c1/c));
+
+    return 0;
+}
+
