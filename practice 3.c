7e0ffb6e2c2cdb676c6707cbc2e586fbf68e6bd5
Baishakh14 <146338298+Baishakh14@users.noreply.github.com>
2024-02-03 00:17:38 +0600

    3300 - Recharged Unlucky Numbers
    3300 - Recharged Unlucky Numbers

---
+++ practice 3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   char a[10001];
+   scanf("%[^\n]%*c",a);
+   if(strstr(a,"13")!=NULL) printf("%s es de Mala Suerte\n",a);
+   else printf("%s NO es de Mala Suerte\n",a);
+    return 0;
+}
