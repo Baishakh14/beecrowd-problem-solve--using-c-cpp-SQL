4aea976ed1c614ab82d33b32c5517e9612ea77a1
Baishakh14 <146338298+Baishakh14@users.noreply.github.com>
2024-02-27 00:31:20 +0600

    2840 - Balloon++
    2840 - Balloon++

---
+++ beecrowd 2840.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    double c;
+    cin>>a>>b;
+    c = 4*(3.1415*a*a*a)/3;
+    int t = b/c;
+    cout<<t<<endl;
+
+    return 0;
+}
